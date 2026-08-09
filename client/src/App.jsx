5a194adfcc3cfd7d4a09fe7ff97f40308c108d7a
Abhishek8827 <01abhishekwani@gmail.com>
2026-08-10 00:10:27 +0530

    Fix React routing for GitHub Pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
 import { useEffect } from "react";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
+
 import AdminLoginPage from "./pages/AdminLoginPage";
 import AdminRequestsPage from "./pages/AdminRequestsPage";
 import HomePage from "./pages/HomePage";
 import TrackRequestPage from "./pages/TrackRequestPage";
+
 import { businessConfig } from "./data/businessConfig";
 
 function App() {
@@ -12,20 +14,28 @@ function App() {
   }, []);
 
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
+
         <Route path="/track-request" element={<TrackRequestPage />} />
+
         <Route path="/admin" element={<AdminLoginPage />} />
+
         <Route path="/admin/requests" element={<ProtectedAdminPage />} />
+
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
 function ProtectedAdminPage() {
-  return localStorage.getItem("girish_admin_token") ? <AdminRequestsPage /> : <Navigate to="/admin" replace />;
+  return localStorage.getItem("girish_admin_token") ? (
+    <AdminRequestsPage />
+  ) : (
+    <Navigate to="/admin" replace />
+  );
 }
 
 export default App;
